3d2daad97ca71c0e8b8780c9ad74d16e324549ab
injea <injea1291@gmail.com>
2024-07-18 20:39:53 +0900

    Refactor main.py and train.py for better code

--- rtdetr_pytorch/tools/train.py
+++ rtdetr_pytorch/tools/train.py
@@ -1,48 +1,44 @@
-"""by lyuwenyu
-"""
-
 import os 
 import sys 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
-import argparse
-
 import src.misc.dist as dist 
 from src.core import YAMLConfig 
 from src.solver import TASKS
+from src.solver.det_solver import DetSolver
+import rtdetr_pytorch.approximation.utils as utils
 
-
-def main(args, ) -> None:
-    '''main
-    '''
+def main(config, resume, tuning, amp, test_only):
+    #분산 프로세스 초기화
     dist.init_distributed()
 
-    assert not all([args.tuning, args.resume]), \
-        'Only support from_scrach or resume or tuning at one time'
-
+    #첫번재 인자로 받은 설정파일에 이후의 인자들을 merge 하여 설정파일 생성
     cfg = YAMLConfig(
-        args.config,
-        resume=args.resume, 
-        use_amp=args.amp,
-        tuning=args.tuning
-    )
+            config,
+            resume=resume, 
+            use_amp=amp,
+            tuning=tuning
+        )
 
-    solver = TASKS[cfg.yaml_cfg['task']](cfg)
-    
-    if args.test_only:
+    solver = DetSolver(cfg)
+
+    if test_only:
         solver.val()
     else:
         solver.fit()
 
 
 if __name__ == '__main__':
+    utils.print_shape = True
+    #변수 초기화 'Only support from_scrach or resume or tuning at one time'
+    config = 'rtdetr_pytorch/configs/rtdetr/rtdetr_r18vd_6x_coco.yml'  #설정 파일 경로
+    resume = None  # resume = '../checkpoint'
+    tuning = 'https://github.com/lyuwenyu/storage/releases/download/v0.1/rtdetr_r18vd_5x_coco_objects365_from_paddle.pth' # 저장된 가중치 경로
+    amp = True # 자동 혼합 정밀도(Automatic Mixed Precision, AMP) FP16 FP32 섞어서 사용. 메모리 사용 감소, 에너지 사용 감소, 계산 속도 향상의 장점
+    test_only = False
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', '-c', type=str, )
-    parser.add_argument('--resume', '-r', type=str, )
-    parser.add_argument('--tuning', '-t', type=str, )
-    parser.add_argument('--test-only', action='store_true', default=False,)
-    parser.add_argument('--amp', action='store_true', default=False,)
+    main(config, resume, tuning, amp, test_only)
+    
 
-    args = parser.parse_args()
 
-    main(args)
+    
+
