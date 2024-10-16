54293518445b07542310274b84c3e2a97ff49cad
injea <injea1291@gmail.com>
2024-10-16 15:23:57 +0900

    attention weight upscaling with learnable variables

--- research/attention_weight_recycle/function.py
+++ research/attention_weight_recycle/function.py
@@ -216,3 +216,20 @@ class HybridEncoder2(HybridEncoder):
             outs.append(out)
 
         return outs
+    
+import convNd
+
+
+def ConvTranspose4d(in_channels: int, out_channels: int, kernel_size:int=2,
+                    stride:int=1, padding:int = 0, padding_mode: str ="zeros", 
+                    bias: bool = True, groups: int = 1, dilation: int = 1):
+    w = torch.rand(1)[0]
+    if bias:
+        b = torch.zeros(1)[0]
+    return convNd.convNd(in_channels=in_channels, out_channels=out_channels,
+                           num_dims=4,kernel_size=kernel_size, 
+                           stride=(stride,stride,stride,stride), padding=padding, 
+                           padding_mode=padding_mode, output_padding=0,
+                           is_transposed=True, use_bias=bias, groups=groups,  dilation = dilation, 
+                           kernel_initializer=lambda x: torch.nn.init.constant_(x, w),  
+                           bias_initializer=lambda x: torch.nn.init.constant_(x, b))
